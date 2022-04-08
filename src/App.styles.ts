b26c50c0cdc6f695c2fa9edec7e6afcb392d57e9
KKatsuragii <mrcludovico@gmail.com>
2022-04-08 15:27:10 -0300

    base da aplicação feita

---
+++ src/App.styles.ts
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+//div do background
+export const Container = styled.div`
+    background-color: #17181F;
+    color: #787A81;
+    min-height: 100vh;
+`;
+
+//div da centralização do conteúdo
+export const area = styled.div`
+    margin: auto;
+    max-width: 980px;
+    padding: 10px;
+`;
+
+//div do cabeçalho
+export const header = styled.h1`
+    margin: 0;
+    padding: 0;
+    color: #FFF;
+    text-align: center;
+    border-bottom: 1px solid #444;
+    padding-bottom: 20px;
+`;
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import * as C from './App.styles';
+import { Item } from './types/Item';
+import { ListItem } from './components/ListItem';
+import { AddArea } from './components/AddArea';
 
-function App() {
+const App = () => {
+  const [list, setList] = useState<Item[]>([
+    {id: 1, name: 'Adicione mais itens nessa lista', done: false},
+    {id: 2, name: 'Marque as tarefas fianlizadas', done: false}
+  ]);
+
+  const handleAddTask = (taskName: string) => {
+    let newList = [...list];
+    newList.push({
+      id: list.length++,
+      name: taskName,
+      done: false
+    });
+    setList(newList);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <C.Container>
+      <C.area>
+        <C.header>Lista de Tarefas</C.header>
+        <AddArea onEnter={handleAddTask}/>
+
+        {list.map((item, index)=>(
+          <ListItem key={index} item={item} />
+        ))}
+
+      </C.area>
+    </C.Container>
   );
-}
+};
 
 export default App;
